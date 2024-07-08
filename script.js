57fc7122fa958c4eabc6cc54ecd600e02bc17236
YawoSadji <yawo.sadji@gmx.com>
2024-07-07 23:24:34 -0400

    add all files

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    }
+
+    )
+}
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    })
+})
